ccf0e60034fd9ae4169c19b334204ac4d6f67cfd
Vasko Dakov <vdakov@login03.delftblue.tudelft.nl>
2024-05-09 16:41:26 +0200

    =script that loads libraries and just breaks

--- delftsblue-scripts/levir-fcef-train.sh
+++ delftsblue-scripts/levir-fcef-train.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 #SBATCH --job-name="LEVIR-FCEF-Train"
-#SBATCH --time=1:10:00
-#SBATCH --output=howmanygpus.out
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=2
-#SBATCH --partition=gpu
+#SBATCH --time=01:00:00
+#SBATCH --partition=compute
 #SBATCH --mem=16G
 #SBATCH --account=education-eemcs-courses-cse3000
 
+
+module load 2023r1
+module load openmpi
 module load python
 module load py-numpy
 module load py-mpi4py
@@ -18,9 +18,9 @@ module load py-keras-preprocessing
 module load py-matplotlib
 module load py-pip
 module load py-scikit-learn
-module load scipy
 module load py-tqdm
-module load 2023r1 openmpi py-torch
+module load py-torch
 module load cuda/11.6
 
+srun python pip -m install requirements.txt
 srun python levir-train-script.py  > out.log
