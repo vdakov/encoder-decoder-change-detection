fc253ec35393f009849626abb525458ae69521bb
vdakov <V.D.Dakov@student.tudelft.nl>
2024-05-09 16:57:31 +0200

    remove unnecessary dependencies

--- util/preprocess_util.py
+++ util/preprocess_util.py
@@ -1,16 +1,6 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from torch.autograd import Variable
-import torchvision.transforms as tr
-import os
 import numpy as np
 from tqdm import tqdm as tqdm
-import pandas 
-import cv2
-import os
-from math import floor, ceil, sqrt, exp
-
 
 def reshape_for_torch(img):
     """Transpose image for PyTorch coordinates."""
