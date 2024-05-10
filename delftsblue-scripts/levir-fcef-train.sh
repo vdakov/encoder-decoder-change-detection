3e19510e6b1b6921878bba807317ab0c3ecf2f1e
vdakov <V.D.Dakov@student.tudelft.nl>
2024-05-10 11:01:29 +0200

    pytorch script

--- delftsblue-scripts/levir-fcef-train.sh
+++ delftsblue-scripts/levir-fcef-train.sh
@@ -3,25 +3,23 @@
 #SBATCH --job-name="LEVIR-FCEF-Train"
 #SBATCH --ntasks=1
 #SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --mem=16G
 #SBATCH --account=education-eemcs-courses-cse3000
 
 
-module load 2023r1 openmpi
+module load 2023r1 openmpi py-mpi4pypy-torch
 module load cuda/11.6
 module load python
 module load py-numpy
-module load py-mpi4py
-module load cuda
 module load py-keras-preprocessing
 module load py-matplotlib
 module load py-pip
 module load py-scikit-learn
 module load py-tqdm
-module load py-torch
-module load cuda/11.6
 
 srun python pip -m install requirements.txt
 srun python levir-train-script.py  > out.log
